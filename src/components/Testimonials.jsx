fba0cc80c769dfe544a31272da3ad43d807b178e
Anas Hamo <anas.hamo.023@gmail.com>
2022-11-26 20:24:38 +0200

    Update

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -7,23 +7,36 @@ import {testimonials} from '../data'
 const Testimonials = () => {
     const [index,setIndex]=useState(0);
     const {name,quote,job,avatar}=testimonials[index];
+
+    const prveTest=()=>{
+        setIndex(prve=>prve -1);
+        if(index < 0){
+            setIndex(testimonials.length-1);
+        }
+    }
+    const nextTest=()=>{
+        setIndex(prve=>prve +1);
+        if(index>= testimonials.length-1){
+            setIndex(0);
+        }
+    }
   return (
-     <section className="testimonials">
+     <section className="testmonials">
         <div className="container testmonials__container">
-            <SectionHead icon={<ImQuotesLeft/>} title="Testmonials"/>
-            <Card className="testmonials">
-                <div className="testmonials__avatar">
+            <SectionHead icon={<ImQuotesLeft/>} title="Testmonials" className="testmonials__head"/>
+            <Card className="testmonial">
+                <div className="testmonial__avatar">
                     <img src={avatar} alt={name} />
                 </div>
-                <p className="testmonials__quote">
+                <p className="testmonial__quote">
                     {`"${quote}"`}
                 </p>
                 <h5>{name}</h5>
-                <small className="testmonials__title">{job}</small>
+                <small className="testmonial__title">{job}</small>
             </Card>
             <div className="testmonials__btn-container">
-                <button className="testmonials__btn"><IoIosArrowDropleftCircle/></button>
-                <button className="testmonials__btn"><IoIosArrowDroprightCircle/></button>
+                <button className="testmonials__btn" onClick={prveTest}><IoIosArrowDropleftCircle/></button>
+                <button className="testmonials__btn" onClick={nextTest}><IoIosArrowDroprightCircle/></button>
             </div>
         </div>
 
